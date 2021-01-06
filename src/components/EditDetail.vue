cdd0b700ec844d066b43f5347dd75ca3732efb23
Tan Nguyen <vantan939@gmail.com>
2021-01-06 20:51:32 +0700

    Research Emit & push data for each component

---
+++ src/components/EditDetail.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p>Tuoi cua con la: {{ age }}</p>
+        <button>Change age</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'EditDetail',
+    props: {
+        age: Number
+    },
+    data() {
+        return {
+            // msg: 'test'
+        }
+    },
+    methods: {
+       
+    }
+}
+</script>
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p>Tuoi cua con la: {{ age }}</p>
+        <button v-on:click="changeAge()">Change age</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UserDetail',
+    props: {
+        age: Number
+    },
+    data() {
+        return {
+            // msg: 'test'
+        }
+    },
+    methods: {
+        changeAge() {
+            this.age = 20
+            this.$emit('ageWasUpdated', this.age)
+        }
+    }
+}
+</script>
